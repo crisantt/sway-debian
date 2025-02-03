a4c9ffbbaa2e471eac23ca30e3490aef59899f6e
Cris <152991141+crisantt@users.noreply.github.com>
2025-02-03 23:23:40 +0800

    Update fonts.sh

--- scripts/fonts.sh
+++ scripts/fonts.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+sudo apt install lxappearance fonts-dejavu fonts-font-awesome fonts-noto-core fonts-noto-cjk fonts-noto-color-emoji fonts-hack-ttf -y
 
+cd ~/.fonts
+
+fonts=("FiraCode" "JetBrainsMono" "Mononoki" "Terminus" "CommitMono" "Noto")
+
+for font in "${fonts[@]}"; do
+    wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/${font}.zip"
+    unzip -n "${font}.zip"
+done
+
+cd ~/
