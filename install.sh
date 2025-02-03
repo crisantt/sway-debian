c80f9f3d7e0b6aac3f36a7192e4acfd1dab829c0
Cris <152991141+crisantt@users.noreply.github.com>
2025-02-03 19:39:58 +0800

    Update install.sh
    added create directory prompt

--- install.sh
+++ install.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+sudo apt update && sudo apt upgrade -y
+
+mkdir -p ~/.config
+mkdir -p ~/.fonts
+mkdir -p ~/.themes
+mkdir -p ~/.icons
+mkdir -p ~/github
+
+bash ~/github/MyDebian/scripts/packages.sh
+clear
+
+bash ~/github/MyDebian/scripts/sway.sh
+clear
+
+bash ~/github/MyDebian/scripts/fonts.sh
+clear
+
+bash ~/github/MyDebian/scripts/build-from-source.sh
+clear
