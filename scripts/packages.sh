12f891a762409ac386af9f55331e65a6575f7168
Cris <152991141+crisantt@users.noreply.github.com>
2025-02-04 01:16:08 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install build-essential clangd make curl wget pkg-config unzip libinput10 libinput-tools libinput-dev elogind -y
+sudo apt install build-essential clangd make curl wget pkg-config unzip libinput10 libinput-dev -y
