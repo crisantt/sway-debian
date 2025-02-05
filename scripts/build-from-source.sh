793d7cf2ad3ca8e2f87e1a45c9bd1f39529fb682
Cris <152991141+crisantt@users.noreply.github.com>
2025-02-05 11:59:06 +0800

    Update build-from-source.sh

--- scripts/build-from-source.sh
+++ scripts/build-from-source.sh
@@ -50,3 +50,15 @@ echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https:/
     | sudo tee /etc/apt/sources.list.d/vscodium.list
 
 sudo apt update && sudo apt install codium -y
+
+#Zen-Browser (2025-feb-05)
+cd ~/packages
+wget https://github.com/zen-browser/desktop/releases/download/1.7.4b/zen.linux-x86_64.tar.bz2
+tar -zvjf zen.linux-x86_64.tar.bz2
+echo "export PATH=\$PATH:\$HOME/packages/zen" >> $HOME/.bashrc
+source .bashrc
+sudo ln -sf $HOME/packages/zen/zen-bin /usr/local/bin/zen
+
+cd $HOME
+
+echo "DONEEE!!"
